e8de262975cd1c50cf6f410a691fef6007a25008
ANDREA <andreamacias109@gmail.com>
2023-11-14 13:21:17 -0500

    Lec

---
+++ src/app/pages/nombre/nombre.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { NombrePage } from './nombre.page';
+
+describe('NombrePage', () => {
+  let component: NombrePage;
+  let fixture: ComponentFixture<NombrePage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(NombrePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
